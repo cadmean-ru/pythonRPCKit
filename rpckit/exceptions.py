710de323ce313a6afcbedb3c96b46f68a32466ed
Aleksey Kritskov <krickov.aleksey@cadmean.ru>
2020-11-12 16:11:34 +0300

    Fix exception

--- rpckit/exceptions.py
+++ rpckit/exceptions.py
@@ -3,8 +3,11 @@ class RpcException(Exception):
         self.code = code
         self.message = message
 
+    def __str__(self):
+        return self.message
+
 
 class FunctionException(RpcException):
 
     def __init__(self, error):
-        super.__init__(error, f"Function call finished with an error: {error}")
+        super().__init__(error, f"Function call finished with an error: {error}")
